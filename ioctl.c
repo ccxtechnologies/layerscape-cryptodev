f695d626fcb94cd6870803e405a758ae6c8924fc
charles <charles@ccxtechnologies.com>
2026-05-12 11:17:01 -0400

    updates to support kernel version 6.18

--- ioctl.c
+++ ioctl.c
@@ -1177,7 +1177,9 @@ static struct ctl_table verbosity_ctl_dir[] = {
 		.mode           = 0644,
 		.proc_handler   = proc_dointvec,
 	},
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 5, 0))
 	{},
+#endif
 };
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 5, 0))
